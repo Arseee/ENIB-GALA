c60ac583f6544172865ccaa145a649f4af7540cc
Du8z <emilien.dubs72@outlook.fr>
2024-01-15 15:04:05 +0100

    Modification de la taille des flèches du carousel

--- src/components/CarouselV2.jsx
+++ src/components/CarouselV2.jsx
@@ -1,11 +1,13 @@
 // Carousel.js
-import React from 'react';
+import React  from 'react';
 import { CCarousel, CCarouselItem } from '@coreui/react';
 import './Carousel.css'
 
 const Carousel = () => {
+
+
   return (
-    <CCarousel controls indicators transition="crossfade" className="h-screen" interval="false">
+    <CCarousel  controls indicators transition="crossfade" className="h-screen" interval={false} >
       <CCarouselItem>
         {/* Contenu de la première diapositive */}
         <div className="carousel-content bg-blue-500 h-screen flex items-center justify-center">
@@ -14,10 +16,8 @@ const Carousel = () => {
         </div>
       </CCarouselItem>
       <CCarouselItem>
-        {/* Contenu de la deuxième diapositive */}
         <div className="carousel-content bg-purple-500 h-screen flex items-center justify-center">
           <h3 className="text-white">Slide 2</h3>
-          {/* Ajoute du texte et des objets ici */}
         </div>
       </CCarouselItem>
     </CCarousel>
