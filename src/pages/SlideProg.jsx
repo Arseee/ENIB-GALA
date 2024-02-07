2b383dc1bf244a12f6a448f0a746e572e3a24843
Du8z <emilien.dubs72@outlook.fr>
2024-02-07 15:07:27 +0100

    changed design

--- src/pages/SlideProg.jsx
+++ src/pages/SlideProg.jsx
@@ -2,55 +2,47 @@ export default function SlideProg() {
   return (
     <div className="carousel-content bg-black h-screen flex items-center justify-center containers font">
       <div className=" container-info  h-screen">
-        <div className="flex justify-around flex-col">
+        <div className="flex justify-center flex-col">
           <div className="flex items-center justify-center">
-            <p className="text-white text-4xl pt-16 underline underline-offset-8 font-bold ">
+            <p className="text-white text-3xl pt-8 underline underline-offset-8 font-bold ">
               Programme
             </p>
           </div>
-          <div className="items-start justify-start">
-            <ul className="pt-20 text-white space-y-20 pl-12 xl:text-2xl lg:text-xl sm:text-md ">
-              <li className="">
-                Remise des diplômes :
-                <span className="font-bold pl-4 "> 15h - 17h30</span>
-              </li>
-              <li>
-                {" "}
-                Cocktail : <span className="font-bold pl-4">17h30 - 18h45</span>
-              </li>
-              <li>
-                {" "}
-                Repas : <span className="font-bold pl-4"> 19h - 22h</span>
-              </li>
-              <li>
-                {" "}
-                Soirée : <span className="font-bold pl-4">22h - 1H</span>
-              </li>
+          <div className="flex items-center justify-center text-center ">
+            <ul className="pt-16 text-white  text-xl ">
+              <li>Remise des diplômes</li>
+              <li className="py-4 font-bold">15h - 17h30</li>
+              <li className="border-t-2 border-gray-400  w-80 mx-auto bottom-0 left-0 right-0 pt-8"></li>
+              <li>Cocktail</li>
+              <li className="py-4 font-bold">17h30 - 18h45</li>
+              <li className="border-t-2 border-gray-400 w-80 mx-auto bottom-0 left-0 right-0 pt-8"></li>
+              <li>Repas</li>
+              <li className="py-4 font-bold">19h - 22h</li>
+              <li className="border-t-2 border-gray-400 w-80 mx-auto bottom-0 left-0 right-0 pt-8"></li>
+              <li>Soirée</li>
+              <li className="py-4 font-bold">22h - 1H</li>
             </ul>
           </div>
         </div>
       </div>
       <div className="bg-white  white-bar"></div>
       <div className=" container-info  h-screen">
-        <div className="flex justify-around flex-col">
+        <div className="flex justify-center flex-col">
           <div className="flex items-center justify-center">
-            <p className="text-white text-4xl pt-16 underline underline-offset-8 font-bold ">
+            <p className="text-white text-3xl pt-8 underline underline-offset-8 font-bold ">
               Menu
             </p>
           </div>
-          <div className="items-start justify-start">
-            <ul className="pt-20 text-white space-y-20 pl-12 xl:text-2xl lg:text-xl sm:text-md ">
-              <li className="">
-                Entrée :<span className="font-bold pl-4 "> 15h - 17h30</span>
-              </li>
-              <li>
-                {" "}
-                Plat : <span className="font-bold pl-4">17h30 - 18h45</span>
-              </li>
-              <li>
-                {" "}
-                Dessert : <span className="font-bold pl-4"> 19h - 22h</span>
-              </li>
+          <div className="flex items-center justify-center text-center ">
+            <ul className="pt-16 text-white  text-xl ">
+              <li>Entrée</li>
+              <li className="py-10 font-bold">15h - 17h30</li>
+              <li className="border-t-2 border-gray-400  w-80 mx-auto bottom-0 left-0 right-0 pt-10"></li>
+              <li>Plat</li>
+              <li className="py-10 font-bold">17h30 - 18h45</li>
+              <li className="border-t-2 border-gray-400 w-80 mx-auto bottom-0 left-0 right-0 pt-10"></li>
+              <li>Dessert</li>
+              <li className="py-10 font-bold">19h - 22h</li>
             </ul>
           </div>
         </div>
