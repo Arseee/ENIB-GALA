3580f9daacb3e97b4e08960128a5ef4d9edce6d0
Du8z <emilien.dubs72@outlook.fr>
2024-03-01 15:43:06 +0100

    added Slide Creator

---
+++ src/pages/SlideCreator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SlideCreator() {
+  return (
+    <div className="carousel-content bg-black  w-full h-screen flex  items-start justify-center">
+      <div className="mt-20 flex flex-col items-center">
+        <p className="text-4xl text-blue-100 font-bold "> Designed By</p>
+        <div className="flex flex-col items-center text-white text-4xl space-y-20 mt-20"></div>
+      </div>
+    </div>
+  );
+}
